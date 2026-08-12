618f8cb3c36bd0f8b12d74e0673772999c7b5838
Softlink <arielgiovanni90@gmail.com>
2026-08-12 17:18:50 -0600

    nueva funcionalidad

---
+++ funcion.py
@@ -0,0 +1,6 @@
+# Imprimir un mensaje en la consola
+print("¡Hola, Mundo!")
+
+# Solicitar datos al usuario y guardarlos en una variable
+nombre = input("¿Cómo te llamas? ")
+print(f"Mucho gusto, {nombre}. ¡Bienvenido a Python!")
